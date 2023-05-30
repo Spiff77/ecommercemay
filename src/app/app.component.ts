1a30d0ce123fde8f3f96b8a78fe134f6f4c039e4
Thomas Lhomme <lhomme.thomas@gmail.com>
2023-05-30 16:28:32 +0300

    toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Product} from './model/product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ecommerce';
+
+  myProduct: Product = {
+    id:2,
+    name:"Cd 2 musik",
+    description: 'un produit qu\'il est bien pour écouter la musique',
+    promo: .1,
+    active: true,
+    category: 'Media',
+    price: 10
+  }
+
+  toggleActive() {
+    this.myProduct.active = !this.myProduct.active
+  }
 }
---
+++ src/app/model/product.model.ts
@@ -0,0 +1,9 @@
+export interface Product {
+  id: number,
+  name: string,
+  active: boolean,
+  description: string,
+  category: string,
+  promo: number,
+  price: number,
+}
