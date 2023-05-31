9a7293404d9202258372db25d582186dc9f11a3b
Thomas Lhomme <lhomme.thomas@gmail.com>
2023-05-31 10:34:03 +0300

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
+import { MenuComponent } from './menu/menu.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ProductListComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule
